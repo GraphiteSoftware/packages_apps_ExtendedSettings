25c696901d9bd822071678ae5226e23fc1139f3d
Anna Fang <anna.fang@graphitesoftware.com>
2017-10-26 09:46:52 -0400

    DEV-3052: Fixed Extended Setting crash issue (not finding class BootBroadcastReceiver)
    More crash fixes

--- Android.mk
+++ Android.mk
@@ -8,6 +8,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := ExtendedSettings
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_DEX_PREOPT := nostripping
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
